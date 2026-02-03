ea3cd3b45ddd454936c8628b74e870d5f279fd6b
Gimm17 <agimpratamazte@gmail.com>
2026-02-03 14:40:45 +0800

    Fix QR scan route - move /t/{token} to web.php root level

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\Cafe\TableSessionController;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
+
+// Table QR entry point (must be at root level for QR scan)
+Route::get('/t/{token}', [TableSessionController::class, 'enter'])->name('cafe.table.enter');
