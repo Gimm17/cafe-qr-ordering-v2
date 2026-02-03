699b7122fada28b197e2f0c34d52e7ffc14130f8
Gimm17 <agimpratamazte@gmail.com>
2026-02-03 14:34:49 +0800

    Fix QrService to use endroid/qr-code v6 API

--- app/Services/QrService.php
+++ app/Services/QrService.php
@@ -2,19 +2,30 @@
 
 namespace App\Services;
 
-use Endroid\QrCode\Builder\Builder;
+use Endroid\QrCode\QrCode;
 use Endroid\QrCode\Writer\PngWriter;
+use Endroid\QrCode\Color\Color;
+use Endroid\QrCode\Encoding\Encoding;
+use Endroid\QrCode\ErrorCorrectionLevel;
+use Endroid\QrCode\RoundBlockSizeMode;
 
 class QrService
 {
     public function png(string $data, int $size = 320): string
     {
-        $result = Builder::create()
-            ->writer(new PngWriter())
-            ->data($data)
-            ->size($size)
-            ->margin(10)
-            ->build();
+        $qrCode = new QrCode(
+            data: $data,
+            encoding: new Encoding('UTF-8'),
+            errorCorrectionLevel: ErrorCorrectionLevel::Medium,
+            size: $size,
+            margin: 10,
+            roundBlockSizeMode: RoundBlockSizeMode::Margin,
+            foregroundColor: new Color(0, 0, 0),
+            backgroundColor: new Color(255, 255, 255)
+        );
+
+        $writer = new PngWriter();
+        $result = $writer->write($qrCode);
 
         return $result->getString(); // PNG binary
     }
