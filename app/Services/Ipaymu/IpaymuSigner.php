63b5c141131a6909d5e4d7bd7043494d228381a5
Gimm17 <agimpratamazte@gmail.com>
2026-02-03 15:14:34 +0800

    Fix iPaymu signature format: VA:BodyHash:Method:ApiKey

--- app/Services/Ipaymu/IpaymuSigner.php
+++ app/Services/Ipaymu/IpaymuSigner.php
@@ -6,14 +6,15 @@ class IpaymuSigner
 {
     /**
      * Signature v2 (iPaymu):
-     * StringToSign = HTTPMethod:VaNumber:lowercase(sha256(RequestBodyJson)):ApiKey
+     * StringToSign = VaNumber:lowercase(sha256(RequestBodyJson)):HTTPMethod:ApiKey
      * Signature = HMAC-SHA256(StringToSign, ApiKey)
      */
     public function makeSignature(string $httpMethod, string $va, string $apiKey, string $jsonBody): string
     {
         $method = strtoupper($httpMethod);
         $bodyHash = strtolower(hash('sha256', $jsonBody));
-        $stringToSign = "{$method}:{$va}:{$bodyHash}:{$apiKey}";
+        // Format: VA:BodyHash:HTTPMethod:APIKey
+        $stringToSign = "{$va}:{$bodyHash}:{$method}:{$apiKey}";
         return hash_hmac('sha256', $stringToSign, $apiKey);
     }
 
